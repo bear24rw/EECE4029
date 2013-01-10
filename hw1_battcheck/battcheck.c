aea83fc750fcf125bbfe3683cb89fcb3fc365f7f
Max Thrun <bear24rw@gmail.com>
2013-01-10 02:28:47 -0500

    store battery structs in a linked list

--- hw1_battcheck/battcheck.c
+++ hw1_battcheck/battcheck.c
@@ -28,13 +28,16 @@ MODULE_DEVICE_TABLE(acpi, battery_device_ids);
 ACPI_MODULE_NAME("battcheck");
 
 struct acpi_battery {
-    struct acpi_device *device;
     int number;
     int state;
     int present_rate;
     int remaining_capacity;
     int present_voltage;
+    struct acpi_device *device;
+    struct list_head list;
 };
+
+struct acpi_battery acpi_battery_list;
 
 size_t info_offsets[] = {
     offsetof(struct acpi_battery, state),
@@ -42,6 +45,8 @@ size_t info_offsets[] = {
     offsetof(struct acpi_battery, remaining_capacity),
     offsetof(struct acpi_battery, present_voltage),
 };
+
+struct task_struct *ts;
 
 /*
  * modified from: drivers/acpi/battery.c
@@ -88,21 +93,24 @@ static int acpi_battery_get_state(struct acpi_battery *battery)
 
 int thread(void *data)
 {
-    struct acpi_battery *battery = (struct acpi_battery*)data;
+    struct acpi_battery *battery = NULL;
+    battery = kzalloc(sizeof(struct acpi_battery), GFP_KERNEL);
 
     while(!kthread_should_stop())
     {
+        list_for_each_entry(battery, &acpi_battery_list.list, list) {
 
-        int result = 0;
-        result = acpi_battery_get_state(battery);
-       
-        printk(KERN_ERR "battcheck: [BAT%d] %5d | %5d | %5d | %5d\n",
-                battery->number,
-                battery->state,
-                battery->present_rate,
-                battery->remaining_capacity,
-                battery->present_voltage
-              );
+            int result = 0;
+            result = acpi_battery_get_state(battery);
+
+            printk(KERN_ERR "battcheck: [BAT%d] %5d | %5d | %5d | %5d\n",
+                    battery->number,
+                    battery->state,
+                    battery->present_rate,
+                    battery->remaining_capacity,
+                    battery->present_voltage
+                  );
+        }
 
         set_current_state(TASK_INTERRUPTIBLE);
         schedule_timeout(HZ);
@@ -111,7 +119,6 @@ int thread(void *data)
     return 0;
 }
 
-struct task_struct *ts[2];
 
 static int acpi_battery_add(struct acpi_device *device)
 {
@@ -138,7 +145,9 @@ static int acpi_battery_add(struct acpi_device *device)
         return 1;
     }
 
-    ts[battery_count] = kthread_run(thread, battery, "battcheck_thread");
+    INIT_LIST_HEAD(&battery->list);
+    list_add_tail(&(battery->list), &(acpi_battery_list.list));
+
 
     battery_count++;
 
@@ -181,15 +190,28 @@ static void __init init_battcheck_async(void *unused, async_cookie_t cookie)
 static int __init init_battcheck(void) 
 {
     async_schedule(init_battcheck_async, NULL);
+
+    INIT_LIST_HEAD(&acpi_battery_list.list); // LIST_HEAD(acpi_battery_list)
+
+    ts = kthread_run(thread, NULL, "battcheck_thread");
+
     printk(KERN_INFO "battcheck: Module loaded successfully\n");
     return 0;
 }
 
 static void __exit unload_battcheck(void) 
 {
-    kthread_stop(ts[0]);
-    kthread_stop(ts[1]);
+    struct acpi_battery *battery, *tmp;
+
+    kthread_stop(ts);
     acpi_bus_unregister_driver(&battcheck_driver);
+
+    list_for_each_entry_safe(battery, tmp, &acpi_battery_list.list, list) {
+        printk(KERN_INFO "freeing node %d\n", battery->number);
+        list_del(&battery->list);
+        kfree(battery);
+    }
+
     printk(KERN_INFO "battcheck: Module unloaded successfully\n");
 }
 
