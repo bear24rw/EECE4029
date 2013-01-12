df997a37c84b3f670d0ebe15f1a43804dbcba0ad
Max Thrun <bear24rw@gmail.com>
2013-01-12 02:58:43 -0500

    generic get control method function

--- hw1_battcheck/battcheck.c
+++ hw1_battcheck/battcheck.c
@@ -2,37 +2,89 @@
 #include <linux/kthread.h>
 #include <acpi/acpi_drivers.h>
 
+/*
+ * For each battery we keep a struct containing
+ * various control method results as well as
+ * the acpi_handle for the device
+ */
 struct acpi_battery {
+
+    /* _BST */
     int state;
     int present_rate;
     int remaining_capacity;
     int present_voltage;
+
+    /* _BIF */
+    int power_unit;
+    int design_capacity;
+    int full_charge_capacity;
+    int technology;
+    int design_voltage;
+    int design_capacity_warning;
+    int design_capacity_low;
+    int capacity_granularity_1;
+    int capacity_granularity_2;
+	char model_number[32];
+	char serial_number[32];
+	char type[32];
+	char oem_info[32];
+
+    acpi_handle handle;
     struct acpi_buffer name;
     struct list_head list;
-    acpi_handle handle;
 };
 
+/* head of battery linked list */
 struct acpi_battery acpi_battery_list;
 
-size_t info_offsets[] = {
-    offsetof(struct acpi_battery, state),
-    offsetof(struct acpi_battery, present_rate),
-    offsetof(struct acpi_battery, remaining_capacity),
-    offsetof(struct acpi_battery, present_voltage),
+/*
+ * When extracting the control method object we need to
+ * be able to lookup where to put the results in our
+ * acpi_battery struct. The following tables are modified
+ * from: drivers/acpi/battery.c
+ */
+struct acpi_offsets {
+	size_t offset;      /* offset inside struct acpi_battery */
+	u8 mode;		    /* int or string? */
+};
+
+/* _BST offsets */
+static struct acpi_offsets bst_offsets[] = {
+    {offsetof(struct acpi_battery, state), 0},
+    {offsetof(struct acpi_battery, present_rate), 0},
+    {offsetof(struct acpi_battery, remaining_capacity), 0},
+    {offsetof(struct acpi_battery, present_voltage), 0},
+};
+
+/* _BIF offsets */
+static struct acpi_offsets bif_offsets[] = {
+	{offsetof(struct acpi_battery, power_unit), 0},
+	{offsetof(struct acpi_battery, design_capacity), 0},
+	{offsetof(struct acpi_battery, full_charge_capacity), 0},
+	{offsetof(struct acpi_battery, technology), 0},
+	{offsetof(struct acpi_battery, design_voltage), 0},
+	{offsetof(struct acpi_battery, design_capacity_warning), 0},
+	{offsetof(struct acpi_battery, design_capacity_low), 0},
+	{offsetof(struct acpi_battery, capacity_granularity_1), 0},
+	{offsetof(struct acpi_battery, capacity_granularity_2), 0},
+	{offsetof(struct acpi_battery, model_number), 1},
+	{offsetof(struct acpi_battery, serial_number), 1},
+	{offsetof(struct acpi_battery, type), 1},
+	{offsetof(struct acpi_battery, oem_info), 1},
 };
 
 struct task_struct *ts;
 
 /*
  * Unpacks the acpi_object into the battery struct
- * Modified from: drivers/acpi/battery.c
+ * Taken from: drivers/acpi/battery.c
  */
 static int extract_package(struct acpi_battery *battery,
         union acpi_object *package,
-        size_t *offsets, int num)
+        struct acpi_offsets *offsets, int num)
 {
     int i;
-    int *x;
     union acpi_object *element;
     if (package->type != ACPI_TYPE_PACKAGE)
         return -EFAULT;
@@ -40,37 +92,71 @@ static int extract_package(struct acpi_battery *battery,
         if (package->package.count <= i)
             return -EFAULT;
         element = &package->package.elements[i];
-        x = (int *)((u8 *)battery + offsets[i]);
-        *x = (element->type == ACPI_TYPE_INTEGER) ?
-            element->integer.value : -1;
+        if (offsets[i].mode) {
+            u8 *ptr = (u8 *)battery + offsets[i].offset;
+            if (element->type == ACPI_TYPE_STRING ||
+                    element->type == ACPI_TYPE_BUFFER)
+                strncpy(ptr, element->string.pointer, 32);
+            else if (element->type == ACPI_TYPE_INTEGER) {
+                strncpy(ptr, (u8 *)&element->integer.value,
+                        sizeof(u64));
+                ptr[sizeof(u64)] = 0;
+            } else
+                *ptr = 0; /* don't have value */
+        } else {
+            int *x = (int *)((u8 *)battery + offsets[i].offset);
+            *x = (element->type == ACPI_TYPE_INTEGER) ?
+                element->integer.value : -1;
+        }
     }
     return 0;
 }
 
 /*
- * Gets updated battery status (_BST) information
+ * Performs a control method and stores the result into the battery sturct
  */
-static int get_battery_status(struct acpi_battery *battery)
+static int get_battery_control_method(struct acpi_battery *battery, const char *method, 
+                                        struct acpi_offsets *offsets, size_t num_offsets)
 {
-    int result = 0;
-    acpi_status status = 0;
+    int result;
+    acpi_status status;
+    acpi_handle handle;
     struct acpi_buffer buffer = { ACPI_ALLOCATE_BUFFER, NULL };
 
-    status = acpi_evaluate_object(battery->handle, NULL, NULL, &buffer);
+    /* get method handle */
+    status = acpi_get_handle(battery->handle, "_BST", &handle);
 
     if (ACPI_FAILURE(status)) {
-        printk(KERN_ERR "battcheck: Error evaluating _BST");
+        printk(KERN_ERR "battcheck: Error get _BST handle\n");
         return -ENODEV;
     }
 
+    /* get method object */
+    status = acpi_evaluate_object(handle, NULL, NULL, &buffer);
+
+    if (ACPI_FAILURE(status)) {
+        printk(KERN_ERR "battcheck: Error evaluating _BST\n");
+        return -ENODEV;
+    }
+
+    /* extract the object into the battery struct */
     result = extract_package(battery, buffer.pointer, 
-            info_offsets, ARRAY_SIZE(info_offsets));
+            offsets, num_offsets);
 
     kfree(buffer.pointer);
 
     return result;
 }
 
+/*
+ * Monitor battery status periodically (default 1 second)
+ * and report the following conditions:
+ * 1) the battery begins to discharge
+ * 2) when it has begun to charge or 
+ * 3) when it has reached full charge 
+ * 4) when the battery level has become low (only one report) 
+ * 5) when the battery level becomes critical (report every 30 seconds)
+ */
 int check_thread(void *data)
 {
     int result;
@@ -86,7 +172,7 @@ int check_thread(void *data)
         list_for_each_entry(battery, &acpi_battery_list.list, list) {
 
             /* get updated battery information */
-            result = get_battery_status(battery);
+            result = get_battery_control_method(battery, "_BST", bst_offsets, ARRAY_SIZE(bst_offsets));
 
             printk(KERN_ERR "battcheck: [%s] %5d | %5d | %5d | %5d\n",
                     (char*)(battery->name).pointer,
@@ -158,12 +244,15 @@ int find_batteries(void)
             if (!battery) return -ENOMEM;
 
             /* set the handle for this battery */
-            battery->handle = rethandle;
+            battery->handle = chandle;
 
             /* get the battery name */
             battery->name.length = ACPI_ALLOCATE_BUFFER;
             battery->name.pointer = NULL;
             acpi_get_name(chandle, ACPI_SINGLE_NAME, &(battery->name));
+
+            /* get the static information from _BIF */
+            get_battery_control_method(battery, "_BIF", bif_offsets, ARRAY_SIZE(bif_offsets));
 
             /* add it to the list */
             INIT_LIST_HEAD(&battery->list);
