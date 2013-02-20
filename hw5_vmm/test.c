733612f60249928f80822f86fdffe52515cef763
Max Thrun <bear24rw@gmail.com>
2013-02-20 16:27:02 -0500

    ioctl working

---
+++ hw5_vmm/test.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include "vmm.h"
+
+int vmm_alloc(int fd, int bytes)
+{
+    int idx = 0;
+
+    idx = ioctl(fd, IOCTL_ALLOC, bytes);
+    if (idx < 0) {
+        printf("vmm_alloc failed: %d\n", idx);
+        exit(-1);
+    }
+
+    return idx;
+}
+
+void vmm_free(int fd, int idx)
+{
+    int rt = ioctl(fd, IOCTL_FREE, idx);
+    if (rt < 0) {
+        printf("vmm_free failed: %d\n", rt);
+        exit(-1);
+    }
+}
+
+int vmm_write(int fd, int idx, char *buf)
+{
+    int rt = 0;
+
+    rt = ioctl(fd, IOCTL_SET_IDX, idx);
+    if (rt < 0) {
+        printf("vmm_set_idx %d failed\n", idx);
+        exit(-1);
+    }
+
+    rt = ioctl(fd, IOCTL_WRITE, buf);
+
+    return rt;
+}
+
+int vmm_read(int fd, int idx, char *buf, int size)
+{
+    int rt = 0;
+
+    rt = ioctl(fd, IOCTL_SET_IDX, idx);
+    if (rt < 0) {
+        printf("vmm_set_idx %d failed\n", idx);
+        exit(-1);
+    }
+
+    rt = ioctl(fd, IOCTL_SET_READ_SIZE, size);
+    if (rt < 0) {
+        printf("vmm_set_read_size %d failed\n", size);
+        exit(-1);
+    }
+
+    rt = ioctl(fd, IOCTL_READ, buf);
+
+    return rt;
+}
+
+
+int main(void)
+{
+    int fd;
+
+    fd = open("/dev/vmm", 0);
+    if (fd < 0) {
+        printf("Can't open device file\n");
+        exit(-1);
+    }
+
+    char buf[5] = "max!\0";
+    char out[5];
+    out[4] = '\0';
+
+    printf("Allocated idx %d\n", vmm_alloc(fd, 100));
+    printf("Writing to idx 5: %d\n", vmm_write(fd, 5, buf));
+    printf("Reading from idx 5: %d\n", vmm_read(fd, 5, out, 4));
+    printf("Read: %s\n", out);
+    printf("Freeing\n"); vmm_free(fd, 5);
+
+    return 0;
+}
+
---
+++ hw5_vmm/vmm.c
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2013 Max Thrun
+ *
+ * EECE4029 - Introduction to Operating Systems
+ * Assignment 5 - Virtual Memory Management
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/vmalloc.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include "vmm.h"
+
+#define POOL_SIZE   16777223
+char *pool;
+
+int current_idx = 0;
+int read_size = 0;
+
+long ioctl(struct file *file,
+           unsigned int ioctl_num,
+           unsigned long ioctl_param)
+{
+    char ch;
+    int size;
+    int page;
+    int bytes;
+
+    switch (ioctl_num) {
+        case IOCTL_ALLOC:
+            size = (int)ioctl_param;
+            printk(KERN_INFO "vmm: allocating %d bytes\n", size);
+            return 0xbeef;
+            break;
+
+        case IOCTL_FREE:
+            page = (int)ioctl_param;
+            printk(KERN_INFO "vmm: freeing idx %d\n", page);
+            break;
+
+        case IOCTL_SET_IDX:
+            current_idx = (int)ioctl_param;
+            printk(KERN_INFO "vmm: setting idx to %d\n", current_idx);
+            break;
+
+        case IOCTL_SET_READ_SIZE:
+            read_size = (int)ioctl_param;
+            printk(KERN_INFO "vmm: setting read size to %d\n", read_size);
+            break;
+
+        case IOCTL_WRITE:
+            bytes = 0;
+            while (1) {
+                get_user(ch, (char*)ioctl_param+bytes);
+                if (ch == '\0') {
+                    printk(KERN_INFO "vmm: wrote 0 bytes\n");
+                    break;
+                }
+                printk(KERN_INFO "writing %c to %d\n", ch, current_idx+bytes);
+                *(pool+current_idx+bytes) = ch;
+                bytes++;
+            }
+            printk(KERN_INFO "vmm: wrote %d bytes\n", bytes);
+            return bytes;
+
+        case IOCTL_READ:
+            bytes = 0;
+            while (bytes < read_size) {
+                put_user(*(pool+current_idx+bytes), (char*)ioctl_param+bytes);
+                bytes++;
+            }
+            printk(KERN_INFO "vmm: read %d bytes\n", bytes);
+            return bytes;
+    }
+
+    return 0;
+}
+
+static int open(struct inode *inode, struct file *file) {printk(KERN_INFO "vmm: open\n"); return 0;}
+static int release(struct inode *inode, struct file *file) {printk(KERN_INFO "vmm: release\n"); return 0;}
+static ssize_t read(struct file *file,
+        char __user * buffer,/* buffer to be filled with data */
+        size_t length,/* length of the buffer     */
+        loff_t * offset)
+{
+    printk(KERN_INFO "vmm: read\n");
+    return 0;
+}
+static ssize_t write(struct file *file,
+        const char __user *buffer,
+        size_t length,
+        loff_t *offset)
+{
+    printk(KERN_INFO "vmm: write\n");
+    return 0;
+}
+
+
+struct file_operations file_ops = {
+    .read = read,
+    .write = write,
+    .unlocked_ioctl = ioctl,
+    .open = open,
+    .release = release
+};
+
+/*
+ * Module entry point
+ */
+static int __init init_mod(void)
+{
+    int ret;
+    ret = register_chrdev(MAJOR_NUM, DEVICE_NAME, &file_ops);
+    if (ret < 0) {
+        printk(KERN_INFO "vmm: could not register the device (error: %d)\n", ret);
+        return ret;
+    }
+
+    pool = vmalloc(POOL_SIZE);
+    if (!pool) {
+        printk(KERN_INFO "vmm: could not allocate pool\n");
+        return -ENOMEM;
+    }
+
+    printk(KERN_INFO "vmm: Module loaded successfully (device number: %d)\n", MAJOR_NUM);
+    return 0;
+}
+
+/*
+ * Module exit point
+ */
+static void __exit exit_mod(void)
+{
+    vfree(pool);
+    unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
+    printk(KERN_INFO "vmm: Module unloaded successfully\n");
+}
+
+module_init(init_mod);
+module_exit(exit_mod);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Max Thrun");
+MODULE_DESCRIPTION("Virtual Memory Manager");
