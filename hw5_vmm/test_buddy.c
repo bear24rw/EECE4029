7d3e15bc468c43194d95ce5e64b5ea038e782c0c
Max Thrun <bear24rw@gmail.com>
2013-02-21 15:11:55 -0500

    more unit tests

--- hw5_vmm/test_buddy.c
+++ hw5_vmm/test_buddy.c
@@ -9,47 +9,163 @@ void alloc_check(int bytes, int idx)
 {
     int rt = buddy_alloc(pool_head, bytes, 0);
     assert(rt == idx);
-    printf("ADDED %d BYTES\t", bytes);
-    printf("|");
+    printf("ADDED %d BYTES\t|", bytes);
     print_tree(pool_head);
+    if (rt < 0) printf("  <-- returned %d", rt);
     printf("\n");
 }
 
-void free_check(int idx, int rt)
+void free_check(int idx, int expected_rt)
 {
-    int ret = buddy_free(pool_head, idx, 0);
-    assert(ret == rt);
-    printf("FREED IDX %d\t", idx);
-    printf("|");
+    int rt = buddy_free(pool_head, idx, 0);
+    assert(rt == expected_rt);
+    printf("FREED IDX %d\t|", idx);
     print_tree(pool_head);
+    if (rt < 0) printf("  <-- returned %d", rt);
     printf("\n");
 }
 
-int main(void)
+void new_pool(int size)
 {
-    pool = malloc(POOL_SIZE);
+    if (pool) free(pool);
+
+    pool = malloc(size);
     if (!pool) {
         printf("vmm: could not allocate pool\n");
-        return 1;
+        exit(-1);
     }
 
     pool_head = (pair_t*)malloc(sizeof(pair_t));
     pool_head->left = NULL;
     pool_head->right = NULL;
     pool_head->state = FREE;
-    pool_head->size = POOL_SIZE;
+    pool_head->size = size;
     pool_head->idx = 0;
+}
 
-    // num_bytes, resulting_idx
+void banner(const char *s)
+{
+    printf("---------- %s ------------\n", s);
+}
+
+
+int main(void)
+{
+    banner("PDF");
+    new_pool(16);
     alloc_check(2, 0);
     alloc_check(4, 4);
     alloc_check(2, 2);
     alloc_check(2, 8);
-
     free_check(4, 0);
     free_check(8, 0);
     free_check(0, 0);
     free_check(2, 0);
+
+    banner("Fill");
+    new_pool(16);
+    alloc_check(2, 0);
+    alloc_check(4, 4);
+    alloc_check(2, 2);
+    alloc_check(2, 8);
+    alloc_check(4, 12);
+    alloc_check(2, 10);
+    free_check(4, 0);
+    free_check(8, 0);
+    free_check(0, 0);
+    free_check(2, 0);
+
+    banner("no space");
+    new_pool(16);
+    alloc_check(2, 0);
+    alloc_check(4, 4);
+    alloc_check(2, 2);
+    alloc_check(2, 8);
+    alloc_check(4, 12);
+    alloc_check(2, 10);
+    alloc_check(2, -1);
+
+    banner("no split");
+    new_pool(16);
+    alloc_check(16, 0);
+    free_check(0, 0);
+
+    banner("remove non existent");
+    new_pool(16);
+    alloc_check(2, 0);
+    alloc_check(4, 4);
+    free_check(2, -1);
+    free_check(8, -1);
+
+    banner("two even");
+    new_pool(16);
+    alloc_check(8, 0);
+    alloc_check(8, 8);
+    free_check(8, 0);
+    free_check(0, 0);
+
+    banner("first too big");
+    new_pool(16);
+    alloc_check(20, -1);
+
+    banner("free empty");
+    new_pool(16);
+    free_check(0, -1);
+    free_check(8, -1);
+
+    banner("free out of range");
+    new_pool(16);
+    alloc_check(2, 0);
+    alloc_check(4, 4);
+    alloc_check(2, 2);
+    free_check(20, -1);
+
+    banner("pool size 1");
+    new_pool(1);
+    alloc_check(2, -1);
+    alloc_check(4, -1);
+    alloc_check(1, 0);
+    free_check(0, 0);
+
+    banner("pool size 0");
+    new_pool(0);
+    alloc_check(2, -1);
+    alloc_check(4, -1);
+    alloc_check(1, -1);
+    free_check(0, -1);
+
+    banner("test 1");
+    new_pool(16);
+    alloc_check(8, 0);
+    alloc_check(2, 8);
+    alloc_check(4, 12);
+    free_check(0, 0);
+    alloc_check(2, 0);
+    alloc_check(2, 2);
+    alloc_check(2, 4);
+    free_check(4, 0);
+    alloc_check(6, -1);
+    alloc_check(4, 4);
+    free_check(0, 0);
+    free_check(2, 0);
+    free_check(4, 0);
+    free_check(12, 0);
+    free_check(8, 0);
+
+    banner("test 2");
+    new_pool(8);
+    alloc_check(8, 0);
+    alloc_check(4, -1);
+    free_check(2, -1);
+    free_check(0, 0);
+    alloc_check(4, 0);
+    alloc_check(2, 4);
+    alloc_check(4, -1);
+    alloc_check(2, 6);
+    free_check(4, 0);
+    free_check(6, 0);
+    free_check(0, 0);
+
 
     return 0;
 }
