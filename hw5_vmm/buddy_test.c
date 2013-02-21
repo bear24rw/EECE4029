e1602d7a8f288d872a75151ad865ea0b79d1ba4f
Max Thrun <bear24rw@gmail.com>
2013-02-21 15:45:33 -0500

    added get_size unit tests

--- hw5_vmm/buddy_test.c
+++ hw5_vmm/buddy_test.c
@@ -7,7 +7,7 @@ char *pool;
 
 void alloc_check(int bytes, int idx)
 {
-    int rt = buddy_alloc(pool_head, bytes, 0);
+    int rt = buddy_alloc(pool_head, bytes);
     assert(rt == idx);
     printf("ADDED %d BYTES\t|", bytes);
     print_tree(pool_head);
@@ -17,13 +17,21 @@ void alloc_check(int bytes, int idx)
 
 void free_check(int idx, int expected_rt)
 {
-    int rt = buddy_free(pool_head, idx, 0);
+    int rt = buddy_free(pool_head, idx);
     assert(rt == expected_rt);
     printf("FREED IDX %d\t|", idx);
     print_tree(pool_head);
     if (rt < 0) printf("  <-- returned %d", rt);
     printf("\n");
 }
+
+void size_check(int idx, int size)
+{
+    printf("SIZE IDX %d == %d\n", idx, size);
+    int rt = buddy_get_size(pool_head, idx);
+    assert(rt == size);
+}
+
 
 void new_pool(int size)
 {
@@ -45,7 +53,8 @@ void new_pool(int size)
 
 void banner(const char *s)
 {
-    printf("---------- %s ------------\n", s);
+    static int i = 1;
+    printf("------------ Test %d: %s ------------\n", i++, s);
 }
 
 
@@ -165,6 +174,58 @@ int main(void)
     free_check(4, 0);
     free_check(6, 0);
     free_check(0, 0);
+
+    banner("size check PDF");
+    new_pool(16);
+    alloc_check(2, 0);
+    alloc_check(4, 4);
+    alloc_check(2, 2);
+    alloc_check(2, 8);
+    size_check(0, 2);
+    size_check(4, 4);
+    size_check(2, 2);
+    size_check(8, 2);
+    free_check(4, 0);
+    free_check(8, 0);
+    free_check(0, 0);
+    free_check(2, 0);
+
+    banner("size check test 1");
+    new_pool(16);
+    alloc_check(8, 0);
+    alloc_check(2, 8);
+    alloc_check(4, 12);
+    free_check(0, 0);
+    size_check(0, -1);
+    size_check(8, 2);
+    size_check(12, 4);
+    alloc_check(2, 0);
+    alloc_check(2, 2);
+    alloc_check(2, 4);
+    size_check(12, 4);
+    size_check(0, 2);
+    size_check(2, 2);
+    size_check(4, 2);
+    free_check(4, 0);
+    size_check(4, -1);
+
+    banner("size check pool size 1");
+    new_pool(1);
+    alloc_check(1, 0);
+    size_check(0, 1);
+    free_check(0, 0);
+    size_check(0, -1);
+
+    banner("size check two even");
+    new_pool(16);
+    alloc_check(8, 0);
+    alloc_check(8, 8);
+    size_check(0, 8);
+    size_check(8, 8);
+    free_check(8, 0);
+    free_check(0, 0);
+    size_check(0, -1);
+    size_check(8, -1);
 
 
     return 0;
