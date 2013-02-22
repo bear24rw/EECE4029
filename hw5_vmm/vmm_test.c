44308b46b43b6680a254315b52da11dee508394a
Max Thrun <bear24rw@gmail.com>
2013-02-21 20:07:49 -0500

    vmm_test output now matches assignment example

--- hw5_vmm/vmm_test.c
+++ hw5_vmm/vmm_test.c
@@ -5,59 +5,43 @@
 #include <sys/ioctl.h>
 #include "vmm.h"
 
-int vmm_alloc(int fd, int bytes)
+int get_mem(int fd, int bytes)
 {
-    int idx = 0;
-
-    idx = ioctl(fd, IOCTL_ALLOC, bytes);
-    if (idx < 0) {
-        printf("vmm_alloc failed: %d\n", idx);
-        exit(-1);
-    }
-
-    return idx;
+    return ioctl(fd, IOCTL_ALLOC, bytes);
 }
 
-void vmm_free(int fd, int idx)
+int free_mem(int fd, int idx)
 {
-    int rt = ioctl(fd, IOCTL_FREE, idx);
-    if (rt < 0) {
-        printf("vmm_free failed: %d\n", rt);
-        exit(-1);
-    }
+    return ioctl(fd, IOCTL_FREE, idx);
 }
 
-int vmm_write(int fd, int idx, char *buf)
+int write_mem(int fd, int idx, char *buf)
 {
     int rt = 0;
 
+    /* set the page we want to write to */
     rt = ioctl(fd, IOCTL_SET_IDX, idx);
-    if (rt < 0) {
-        printf("vmm_set_idx %d failed\n", idx);
-        exit(-1);
-    }
+    if (rt < 0) return rt;
 
+    /* execute the write */
     rt = ioctl(fd, IOCTL_WRITE, buf);
 
     return rt;
 }
 
-int vmm_read(int fd, int idx, char *buf, int size)
+int read_mem(int fd, int idx, char *buf, int size)
 {
     int rt = 0;
 
+    /* set the page we want to read from */
     rt = ioctl(fd, IOCTL_SET_IDX, idx);
-    if (rt < 0) {
-        printf("vmm_set_idx %d failed\n", idx);
-        exit(-1);
-    }
+    if (rt < 0) return rt;
 
+    /* set how many bytes we want to read */
     rt = ioctl(fd, IOCTL_SET_READ_SIZE, size);
-    if (rt < 0) {
-        printf("vmm_set_read_size %d failed\n", size);
-        exit(-1);
-    }
+    if (rt < 0) return rt;
 
+    /* execute the read */
     rt = ioctl(fd, IOCTL_READ, buf);
 
     return rt;
@@ -66,27 +50,21 @@ int vmm_read(int fd, int idx, char *buf, int size)
 
 int main(void)
 {
-    int fd;
+    int mem, ref;
+    char buffer[4096];
 
-    fd = open("/dev/vmm", 0);
-    if (fd < 0) {
+    mem = open("/dev/vmm", 0);
+    if (mem < 0) {
         printf("Can't open device file\n");
         exit(-1);
     }
 
-    char buf[4] = "max\0";
-    char out[4];
-    out[3] = '\0';
-
-    printf("Allocated idx %d\n", vmm_alloc(fd, 2));
-    printf("Allocated idx %d\n", vmm_alloc(fd, 4));
-    printf("Allocated idx %d\n", vmm_alloc(fd, 2));
-    /*
-    printf("Writing to idx 5: %d\n", vmm_write(fd, 0, buf));
-    printf("Reading from idx 5: %d\n", vmm_read(fd, 0, out, 4));
-    printf("Read: %s\n", out);
-    printf("Freeing\n"); vmm_free(fd, 0);
-    */
+    ref = get_mem(mem, 100);
+    sprintf(buffer, "Hello buddy");
+    write_mem(mem, ref, buffer);
+    read_mem(mem, ref+3, buffer, 10);
+    printf("buffer: %s\n", buffer);
+    free_mem(mem, ref);
 
     return 0;
 }
